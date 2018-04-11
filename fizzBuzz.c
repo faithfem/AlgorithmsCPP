45d5328faa3ae53858dd0ad53edd250546334ede
Faith <fmugure@intracitygeeks.org>
2018-04-11 02:55:17 +0000

    fizzBuzz

---
+++ fizzBuzz.c
@@ -0,0 +1,17 @@
+//#include <iostream>
+#include <stdio.h>
+//using namespace std;
+//using std::cout;
+
+int main(){
+    int num = 30;
+    for(int i=1; i<=num;i++){
+        printf("\n");
+        //cout<< ("\n");
+        if(i%15==0) printf("FizzBuzz");
+        else if(i%3==0) printf("Fizz"); 
+        else if(i%5==0) printf("Buzz"); 
+        //else if(i%!=0) printf(i);
+    }
+    printf("\n");
+}
