57acf66f765464aa957d6d674a2058d5d3eba242
Faith <fmugure@intracitygeeks.org>
2018-04-11 03:02:57 +0000

    fizzBuzz C

--- fizzBuzz.c
+++ fizzBuzz.c
@@ -7,11 +7,10 @@ int main(){
     int num = 30;
     for(int i=1; i<=num;i++){
         printf("\n");
-        //cout<< ("\n");
         if(i%15==0) printf("FizzBuzz");
         else if(i%3==0) printf("Fizz"); 
         else if(i%5==0) printf("Buzz"); 
-        else printf(i);
+        //else printf(i);
     }
     printf("\n");
 }
